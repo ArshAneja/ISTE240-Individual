f720a3de80d9c332463db93f04b3f87fdf8a5efb
ArshAneja <75496064+ArshAneja@users.noreply.github.com>
2022-10-02 17:12:34 -0400

    Add files via upload

---
+++ personal/assets/images/mouseover.js
@@ -0,0 +1,11 @@
+// JavaScript code for Lecture
+
+// First JavaScript function that changes the id content.
+// Parameter passing is the key to understanding this code.
+
+
+function change(id,img) 
+{
+document.getElementById(id).src=img;
+    
+}
